46ef31b8483f36dbf42cc2c20c4983eec475bb04
calebgl <57515964+calebgl@users.noreply.github.com>
2023-05-21 01:09:24 -0600

    keymaps to jump in snippets

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -34,15 +34,15 @@ return {
 
       local cmp = require('cmp')
       local cmp_select = { behavior = cmp.SelectBehavior.Select }
+      local cmp_action = require("lsp-zero").cmp_action()
       local cmp_mappings = lsp.defaults.cmp_mappings({
         ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
         ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
         ["<C-y>"] = cmp.mapping.confirm({ select = true }),
         ["<C-Space>"] = cmp.mapping.complete(),
+        ["<Tab>"] = cmp_action.luasnip_jump_forward(),
+        ["<S-Tab>"] = cmp_action.luasnip_jump_backward(),
       })
-
-      cmp_mappings['<Tab>'] = nil
-      cmp_mappings['<S-Tab>'] = nil
 
       lsp.setup_nvim_cmp({
         mapping = cmp_mappings
