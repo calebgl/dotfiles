84fe1ef588f8b517bc23155a5299604cfb4b337a
calebgl <57515964+calebgl@users.noreply.github.com>
2023-05-21 22:33:43 -0600

    add keymaps to treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,34 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
     build = ":TSUpdate",
+    keys = {
+      { "<leader>tnn" },
+      { "<leader>trn" },
+      { "<leader>trc" },
+      { "<leader>trm" }
+    },
     config = function()
-      require("nvim-treesitter.configs").setup {
+      require("nvim-treesitter.configs").setup({
         auto_install = true,
+        sync_install = false,
         highlight = {
-          enable = true
+          enable = true,
+          additional_vim_regex_highlighting = false,
         },
-      }
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<leader>tnn",
+            node_incremental = "<leader>trn",
+            scope_incremental = "<leader>trc",
+            node_decremental = "<leader>trm",
+          },
+        },
+
+      })
     end
+
   }
 }
