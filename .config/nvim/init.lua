4607c21c8af34be3a11dc64d2d04761d8cea9616
calebgl <57515964+calebgl@users.noreply.github.com>
2023-05-21 22:34:00 -0600

    load opts, autocmds, and keymaps before calling lazy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,20 @@
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath
+  })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
-
-require("config.options")
-require("config.keymaps")
 
 require("lazy").setup("plugins")
