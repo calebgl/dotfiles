6606999fa5250d058187dbefd73fe8a69702d419
calebgl <57515964+calebgl@users.noreply.github.com>
2023-05-29 08:09:23 -0600

    add undotree and change colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,19 @@
 return {
   {
-    'catppuccin/nvim',
+    'folke/tokyonight.nvim',
     lazy = false,
-    name = 'catppuccin',
     opts = {
-      transparent_background = true
+      style = "storm",
+      transparent = true,
+      styles = {
+        floats = "transparent"
+      }
     },
-    config = function()
-      vim.cmd.colorscheme("catppuccin-frappe")
-      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+      vim.cmd.colorscheme("tokyonight-storm")
+      -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end
   }
 }
