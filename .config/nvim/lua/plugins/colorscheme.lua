ec0e66b9b6cca50a7b611327daf5a8e49c8ec51f
calebgl <57515964+calebgl@users.noreply.github.com>
2023-08-21 21:58:42 -0600

    nvim theme change

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,31 @@
 return {
-  {
-    'folke/tokyonight.nvim',
-    lazy = false,
-    opts = {
-      style = "storm",
-      transparent = true,
-      styles = {
-        floats = "transparent"
-      }
-    },
-    config = function(_, opts)
-      require("tokyonight").setup(opts)
-      vim.cmd.colorscheme("tokyonight-storm")
-      -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-      -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    end
-  }
+    -- {
+    --   'folke/tokyonight.nvim',
+    --   lazy = false,
+    --   opts = {
+    --     style = "storm",
+    --     transparent = true,
+    --     styles = {
+    --       floats = "transparent"
+    --     }
+    --   },
+    --   config = function(_, opts)
+    --     require("tokyonight").setup(opts)
+    --     vim.cmd.colorscheme("tokyonight-storm")
+    --     -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    --     -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    --   end
+    -- }
+    {
+        'rose-pine/neovim',
+        lazy = false,
+        opts = {
+            disable_background = true,
+            disable_float_background = true
+        },
+        config = function(_, opts)
+            require('rose-pine').setup(opts)
+            vim.cmd.colorscheme('rose-pine')
+        end
+    }
 }
