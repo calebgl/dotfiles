8db20157fc6a59821add18be859dee8eaf744289
calebgl <57515964+calebgl@users.noreply.github.com>
2023-12-10 19:44:51 -0600

    add fzf to telescope nvim

--- nvim/.config/nvim/lua/calebgl/plugins/editor.lua
+++ nvim/.config/nvim/lua/calebgl/plugins/editor.lua
@@ -4,6 +4,14 @@ return {
 		branch = "0.1.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
+
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+				cond = function()
+					return vim.fn.executable("make") == 1
+				end,
+			},
 		},
 		opts = {
 			defaults = {
@@ -35,12 +43,17 @@ return {
 		},
 		keys = {
 			{ "<leader>ff", "<cmd>Telescope find_files<cr>" },
-			{ "<leader>fh", "<cmd>Telescope find_files<cr>" },
+			{ "<leader>fh", "<cmd>Telescope find_files hidden=true no-ignore=true<cr>" },
 			{ "<leader>fs", "<cmd>Telescope live_grep<cr>" },
 			{ "<leader>fb", "<cmd>Telescope buffers<cr>" },
 			{ "<leader>fi", "<cmd>Telescope current_buffer_fuzzy_find<cr>" },
 			{ "<leader>f:", "<cmd>Telescope command_history<cr>" },
 		},
+        config = function (_, opts)
+            local telescope = require("telescope")
+            telescope.setup(opts)
+            pcall(telescope.load_extension, 'fzf')
+        end
 	},
 
 	{
