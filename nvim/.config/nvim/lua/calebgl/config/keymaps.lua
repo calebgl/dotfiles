d0690a6bdcd0c855fd1a6ada95dab99b751bc876
calebgl <57515964+calebgl@users.noreply.github.com>
2024-01-06 15:03:07 -0600

    change config to keep consistency

--- nvim/.config/nvim/lua/calebgl/config/keymaps.lua
+++ nvim/.config/nvim/lua/calebgl/config/keymaps.lua
@@ -1,31 +1,27 @@
-local keymap = vim.keymap
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
-keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set("n", "<A-j>", "<cmd>m .+1<cr>==")
+vim.keymap.set("n", "<A-k>", "<cmd>m .-2<cr>==")
+vim.keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi")
+vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi")
+vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv")
+vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv")
 
-keymap.set("n", "<A-j>", "<cmd>m .+1<cr>==")
-keymap.set("n", "<A-k>", "<cmd>m .-2<cr>==")
-keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi")
-keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi")
-keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv")
-keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv")
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
 
-keymap.set("v", "<", "<gv")
-keymap.set("v", ">", ">gv")
+vim.keymap.set("n", "J", "mzJ`z")
 
-keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
-keymap.set("n", "<C-d>", "<C-d>zz")
-keymap.set("n", "<C-u>", "<C-u>zz")
-keymap.set("n", "n", "nzzzv")
-keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("x", "<leader>p", '"_dP')
 
-keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
 
-keymap.set("n", "<leader>y", "\"+y")
-keymap.set("v", "<leader>y", "\"+y")
-keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set({ "n", "v" }, "<leader>d", '"_d')
 
-keymap.set("n", "<leader>d", "\"_d")
-keymap.set("v", "<leader>d", "\"_d")
-
-keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "Q", "<nop>")
