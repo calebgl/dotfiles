ed98503a91ef286c991feb2807e1a4c9d7170c3e
calebgl <caleb.csgl@gmail.com>
2024-01-06 14:59:27 -0600

    change autocmds to use camel case

--- nvim/.config/nvim/lua/calebgl/config/autocmds.lua
+++ nvim/.config/nvim/lua/calebgl/config/autocmds.lua
@@ -1,9 +1,9 @@
 local function augroup(name)
-    return vim.api.nvim_create_augroup("calebgl_" .. name, { clear = true })
+    return vim.api.nvim_create_augroup("CalebGL_" .. name, { clear = true })
 end
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-    group = augroup("highlight_yank"),
+    group = augroup("HighlightYank"),
     pattern = "*",
     callback = function()
         vim.highlight.on_yank({
@@ -14,26 +14,26 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-    group = augroup("close_with_q"),
+    group = augroup("CloseWithQ"),
     pattern = {
         "help",
         "man",
         "checkhealth",
-        "fugitive"
+        "fugitive",
     },
     callback = function(event)
         vim.bo[event.buf].buflisted = false
         vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
-    end
+    end,
 })
 
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  group = augroup("auto_create_dir"),
-  callback = function(event)
-    if event.match:match("^%w%w+://") then
-      return
-    end
-    local file = vim.loop.fs_realpath(event.match) or event.match
-    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
-  end,
+vim.api.nvim_create_autocmd("BufWritePre", {
+    group = augroup("AutoCreateDir"),
+    callback = function(event)
+        if event.match:match("^%w%w+://") then
+            return
+        end
+        local file = vim.loop.fs_realpath(event.match) or event.match
+        vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+    end,
 })
