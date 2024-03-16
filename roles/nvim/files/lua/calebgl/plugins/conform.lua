6113f23e621273f323f96eec079b8223826225da
calebgl <57515964+calebgl@users.noreply.github.com>
2024-03-16 11:44:58 -0600

    feat: add nvim config for colorscheme, comment and conform

---
+++ roles/nvim/files/lua/calebgl/plugins/conform.lua
@@ -0,0 +1,30 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			-- python = { "isort", "black" },
+			-- go = { "golines", "gofumpt", "goimports" },
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			css = { "prettier" },
+			json = { "prettier" },
+			-- yaml = { "prettier" },
+			-- astro = { "prettier" },
+		},
+	},
+	keys = {
+		{
+			"<leader>lc",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = { "n", "v" },
+			desc = "Format buffer",
+		},
+	},
+}
