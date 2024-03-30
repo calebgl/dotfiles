71edc437d23be16eb3f45a6316c8bd1cf8bfb664
calebgl <57515964+calebgl@users.noreply.github.com>
2024-03-30 00:53:33 -0600

    refact: move autocmd with the others

--- roles/nvim/files/lua/calebgl/plugins/lsp.lua
+++ roles/nvim/files/lua/calebgl/plugins/lsp.lua
@@ -24,29 +24,6 @@ return {
 	config = function()
 		require("neodev").setup()
 
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("CalebGL_LspConfig", { clear = true }),
-			callback = function(event)
-				local opts = { buffer = event.buf }
-				local builtin = require("telescope.builtin")
-
-				vim.keymap.set("n", "gd", builtin.lsp_definitions, opts)
-				vim.keymap.set("n", "gr", builtin.lsp_references, opts)
-				vim.keymap.set("n", "gI", builtin.lsp_implementations, opts)
-				vim.keymap.set("n", "<leader>D", builtin.lsp_type_definitions, opts)
-				vim.keymap.set("n", "<leader>lds", builtin.lsp_document_symbols, opts)
-				vim.keymap.set("n", "<leader>lws", builtin.lsp_dynamic_workspace_symbols, opts)
-
-				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-				vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
-				vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
-				vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, opts)
-				vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-				vim.keymap.set({ "n", "i" }, "<c-k>", vim.lsp.buf.signature_help, opts)
-			end,
-		})
-
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
