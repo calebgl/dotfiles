8ba1f6a66aac0a0701aae1a0b5d5a89a32c1c49a
calebgl <57515964+calebgl@users.noreply.github.com>
2024-04-17 16:00:19 -0600

    feat: remove bold and enable transparent for nvim

--- roles/nvim/files/lua/calebgl/plugins/colorscheme.lua
+++ roles/nvim/files/lua/calebgl/plugins/colorscheme.lua
@@ -4,6 +4,8 @@ return {
 		priority = 1000,
 		config = function()
 			require("gruvbox").setup({
+				transparent_mode = true,
+				bold = false,
 				italic = {
 					strings = false,
 					emphasis = false,
