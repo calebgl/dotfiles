d0cfaa0ff82c0f2daf1f68f6bc6ad4abdf64cb7e
calebgl <57515964+calebgl@users.noreply.github.com>
2024-06-29 21:25:26 -0600

    feat(nvim): show hidden files within oil

--- roles/nvim/files/lua/calebgl/plugins/oil.lua
+++ roles/nvim/files/lua/calebgl/plugins/oil.lua
@@ -3,7 +3,20 @@ return {
 	opts = {},
 	config = function()
 		require("oil").setup({
-			default_file_explorer = true
+			default_file_explorer = true,
+			view_options = {
+				show_hidden = true,
+
+				is_always_hidden = function(name)
+					return name == "node_modules" or name == "." or name == ".."
+				end,
+
+				natural_order = true,
+				sort = {
+					{ "type", "asc" },
+					{ "name", "asc" }
+				}
+			}
 		})
 
 		vim.keymap.set("n", "<leader>e", "<cmd>Oil<cr>")
