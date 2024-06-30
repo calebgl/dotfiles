519ec29c6961a754f48e8e0731428a35c9b0ab73
calebgl <57515964+calebgl@users.noreply.github.com>
2024-06-29 22:31:19 -0600

    feat(nvim): add treesitter context

--- roles/nvim/files/lua/calebgl/plugins/treesitter.lua
+++ roles/nvim/files/lua/calebgl/plugins/treesitter.lua
@@ -1,21 +1,36 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	opts = {
-		auto_install = true,
-		highlight = { enable = true },
-		indent = { enable = true, disable = { "html" } },
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<c-space>",
-				node_incremental = "<c-space>",
-				scope_incremental = false,
-				node_decremental = "<bs>",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		opts = {
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true, disable = { "html" } },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<c-space>",
+					node_incremental = "<c-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
 			},
 		},
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+		end,
 	},
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
-	end,
+
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		config = function()
+			vim.cmd.highlight("TreesitterContext gui=none")
+			vim.cmd.highlight("TreesitterContextSeparator gui=none")
+			vim.cmd.highlight("TreesitterContextBottom gui=none")
+			require("treesitter-context").setup({
+				max_lines = 4,
+				separator = "-",
+			})
+		end
+	}
 }
